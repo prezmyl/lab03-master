f92d8d40860fb477cf19cf19cf30dbff9b79ca60
xpolas <prezmyl@gmail.com>
2023-10-13 17:04:22 +0200

    osfhos

--- src/main/java/lab/Game.java
+++ src/main/java/lab/Game.java
@@ -36,11 +36,11 @@ public class Game {
 		this.height = height;
 		this.staticObject = new StaticObjects(this);
 		this.bats = new Bat[] {
-			new Bat(this, new Point2D(gap, lineWidth), new Point2D(0,5), sizeBatX, sizeBatY)
+			new Bat(this, new Point2D(gap, lineWidth), new Point2D(0,4.7), sizeBatX, sizeBatY)
 			,new Bat(this, new Point2D(width - (gap + sizeBatX), height - (sizeBatY + lineWidth)), new Point2D(0,-4.5), sizeBatX, sizeBatY)
 		};
 		this.ball = new Ball(this, new Point2D(width/2 - sizeBall/2 ,height/2), new Point2D(6.41,0), sizeBall);
-		this.score1 = new Score(this, new Point2D(width/2 - 3 * gap, height - 2.5 * gap), 0);
+		this.score1 = new Score(this, new Point2D(width/2 - 4 * gap, height - 2.5 * gap), 0);
 		this.score2 = new Score(this, new Point2D(width/2 +  2 * gap, height - 2.5 * gap), 0);
 	}
 
@@ -56,45 +56,52 @@ public class Game {
 	}
 	
 	public void simulate(double timeDelta) {
+		actualTime += timeDelta;
 		for(Bat bat: bats) {
 			bat.simulate(timeDelta);
 		}
 		this.ball.simulate(timeDelta);
 		
-
-		Rectangle2D chOfball = ball.getConvexhall();
-		for (Bat bat : bats) {
-			if (bat.getConvexHall().intersects(chOfball)) {
-				ball.collision();
-				batHitCount++;
-				System.out.println("ballCollision: " + batHitCount);
-				
-				//temporarly
-				if (bat == bats[0]) {
-					score1.addScore();
-				}
-				else {
-					score2.addScore();
+		/*System.out.println("actualT: " + actualTime);
+		System.out.println("lastHit: " + lastHit);
+		System.out.println("actual - lastHit: " + (actualTime - lastHit));*/
+		
+		if (actualTime - lastHit > 5) {
+			Rectangle2D chOfball = ball.getConvexhall();
+			for (Bat bat : bats) {
+				if (bat.getConvexHall().intersects(chOfball)) {
+					ball.collision();
+					batHitCount++;
+					System.out.println("ballCollision: " + batHitCount);
+					
+					//temporarly
+					if (bat == bats[0]) {
+						score1.addScore();
+					}
+					else {
+						score2.addScore();
+					}
+					lastHit = actualTime;
 				}
 			}
-		}
-			
-			
-		if (chOfball.intersects(staticObject.getLeftLine().getConvexHall())) {
+				
+				
+			if (chOfball.intersects(staticObject.getLeftLine().getConvexHall())) {
 				ball.collision();
 				wallHitLeft++;
 				System.out.println("wallLeft: " + wallHitLeft);
-				//score1.subtrScore(); //temporarly
-				
+				score1.subtrScore(); //temporarly
+				lastHit = actualTime;
+				}
+			if (chOfball.intersects(staticObject.getRightLine().getConvexHall())) {
+				ball.collision();
+				wallHitRigtht++;
+				System.out.println("wallRight: " + wallHitRigtht);
+				score2.subtrScore(); //temporarly
+				lastHit = actualTime;
 			}
-		if (chOfball.intersects(staticObject.getRightLine().getConvexHall())) {
-			ball.collision();
-			wallHitRigtht++;
-			System.out.println("wallRight: " + wallHitRigtht);
-			//score2.subtrScore(); //temporarly
+			
 		}
-		
-		
 	}
 	
 	public Point2D transform2Canvas(Point2D worldPoint, double entityHieght) {
